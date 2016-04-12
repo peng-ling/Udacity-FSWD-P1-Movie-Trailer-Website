5dd48c5db89e6fc2f7031e7709e9149c9918a805
peng-ling <paulengling@Pauls-MacBook-Air.local>
2016-04-12 19:42:07 +0200

    ready for review version

---
+++ README.md
@@ -0,0 +1,6 @@
+#Udacity Full Stack Web Developer Nanodegree Proect 1 Movie Trailer Website#
+
+For this Project (Python 2.7.1)[https://docs.python.org/2/index.html] is used
+to generate a Movie Trailer Website.
+
+To let Python generate the Website just run entertainment_center.py 
--- entertainment_center.py
+++ entertainment_center.py
@@ -1,18 +1,47 @@
+# Import the class which generates the html file
+import fresh_tomatoes
+# Import the class which provides the media object which will get initiated for each movie
 import media
 
+# initiate a new instance of media.Movie
 toy_story = media.Movie("Toy Story",
-                        "A story about a boy and his toys which come to live",
-                        "https://en.wikipedia.org/wiki/File:Toy_Story.jpg",
-                        "https://youtu.be/KYz2wyBy3kc")
+            "A story about a boy and his toys which come to live",
+            "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+            "https://youtu.be/KYz2wyBy3kc")
+
 avatar = media.Movie("Avatar",
-                    "A story about a avatars",
-                    "https://en.wikipedia.org/wiki/File:Avatar-Teaser-Poster.jpg",
-                    "https://youtu.be/5PSNL1qE6VY")
+            "A story about a avatars",
+            "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-"
+            "Poster.jpg",
+            "https://youtu.be/5PSNL1qE6VY")
 
 aviator = media.Movie("Aviator",
-                    "Depicts the life of Howard Hughes",
-                    "https://en.wikipedia.org/wiki/File:The_Aviator_Poster.jpg",
-                    "https://youtu.be/zikFDK4cuQA")
+            "Depicts the life of Howard Hughes",
+            "https://upload.wikimedia.org/wikipedia/en/f/f7/The_Aviator_"
+            "Poster.jpg",
+            "https://youtu.be/zikFDK4cuQA")
 
-print aviator.storyline
-aviator.show_trailer()
+monsters_university = media.Movie("Monster University",
+            "Monsters University tells the story of two monsters, Mike and "
+            "Sulley, and their time studying at college",
+            "https://upload.wikimedia.org/wikipedia/en/2/2a/Monsters_University"
+            "_poster_3.jpg",
+            "https://youtu.be/ODePHkWSg-U")
+
+american_beauty = media.Movie("American Beauty",
+                    "A satire of American middle class notions",
+                    "https://upload.wikimedia.org/wikipedia/en/b/b6/American_"
+                    "Beauty_poster.jpg",
+                    "https://youtu.be/FfWnZthD9Tw")
+
+pulp_fiction = media.Movie("Pulp Fiction",
+                    "Intersecting story of Los Angeles mobsters",
+                    "https://upload.wikimedia.org/wikipedia/en/8/82/Pulp_"
+                    "Fiction_cover.jpg",
+                    "https://youtu.be/ewlwcEBTvcg")
+
+#put the media.movie object in an arry
+movies = [toy_story,avatar,aviator, monsters_university, american_beauty,
+          pulp_fiction]
+# call the metahode of fresh_tomatoes which will generate the html file
+fresh_tomatoes.open_movies_page(movies)
